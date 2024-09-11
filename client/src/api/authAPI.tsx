3dc6e078006ab4aaa8887290117a547c6a5a3016
Spencer_Durfey <durfey_32@yahoo.com>
2024-09-10 22:30:53 -0600

    working through the auth todos

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,7 +1,28 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  
+  try {
+    const respones = await fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+    if (!respones.ok) {
+      const errorData = await respones.json();
+      throw new Error(`Error: ${errorData.message}`);
+    }
+
+    const data = await respones.json();
+
+    return data;
+  } catch (error) {
+    console.log('Error from user login', error);
+    return Promise.reject('Could not fetch user info')
+  }
+
   // TODO: make a POST request to the login route
 }
 
